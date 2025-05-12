a4ea6ea7c2c0b188aaecf1c42bd154b473c5c4b9
sanskruti kanthale <sanskruti@samyotech.com>
2025-05-12 16:48:08 +0530

    configration of creating apk

--- app.config.ts
+++ app.config.ts
@@ -9,5 +9,8 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   extra: {
     ENV: process.env.APP_ENV || 'development',
     API_URL: process.env.API_URL,
+    eas: {
+      "projectId": "127528e3-b9c6-42b0-9955-0785b9950879"
+    }
   },
 });
